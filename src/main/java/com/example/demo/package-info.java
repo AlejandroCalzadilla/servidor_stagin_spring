7e233ad07c3bd58a633092996c480f7c87fd05c5
alejandro <78042869ale@gmail.com>
2025-08-24 15:13:55 -0400

    code queality v2

--- src/main/java/com/example/demo/package-info.java
+++ src/main/java/com/example/demo/package-info.java
@@ -1,5 +1,5 @@
-package com.example.demo;
 /**
  * The main package for the demo application.
  * This package contains the main application class and other core components.
  */
+package com.example.demo;
